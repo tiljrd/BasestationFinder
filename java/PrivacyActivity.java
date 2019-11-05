93d093393632cdb97462cd37446526761e1e1839
tiljrd <45370031+tiljrd@users.noreply.github.com>
2019-11-05 14:24:31 +0000

    Create PrivacyActivity.java

---
+++ java/PrivacyActivity.java
@@ -0,0 +1,37 @@
+package com.baselabs.tiljo.basestationfinder;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class PrivacyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy);
+    }
+
+    public void openPrivacyWebsite (View view){
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("https://basestation-finder.jimdofree.com/" +
+                "datenschutzerkl%C3%A4rung-f%C3%BCr-die-app/"));
+        startActivity(intent);
+    }
+
+    public void removePermissions(View view){
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setData(Uri.parse("package:" + getPackageName()));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+        startActivity(intent);
+    }
+}
